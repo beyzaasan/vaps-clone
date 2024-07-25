1fddeddbc87dba311cf5392ddf67434ddc8c4327
Beyza <asanbeyza3@gmail.com>
2024-07-25 14:11:40 +0300

    tamam

--- src/com/gui/Temperature.java
+++ src/com/gui/Temperature.java
@@ -4,11 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
-import java.io.File;
-import javax.imageio.ImageIO;
+import java.io.*;
+import javax.imageio.*;
 
 public class Temperature extends javax.swing.JFrame {
-    
+
     public Temperature(){
         initComponents();
     }
@@ -23,14 +23,21 @@ public class Temperature extends javax.swing.JFrame {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
 
+        JPanel whiteDashPanel = new JPanel();
+        //whiteDashPanel.setLayout(new BorderLayout());
+        whiteDashPanel.setPreferredSize(new Dimension(200, 300));
+
         JLabel whiteDash = new JLabel("|");
         // Set the font size and color
         whiteDash.setFont(new Font("Arial", Font.BOLD, 200)); // Adjust size as needed
         whiteDash.setForeground(Color.WHITE);
-        whiteDash.setLayout(new FlowLayout(FlowLayout.LEFT));
-        mainPanel.add(whiteDash, BorderLayout.WEST);
+        whiteDashPanel.setLayout(new BoxLayout(whiteDashPanel, BoxLayout.Y_AXIS));
+        whiteDashPanel.add(whiteDash, BorderLayout.CENTER);
 
-        JPanel dashPanel = new JPanel();
+        TempScroll tempScroll = new TempScroll(); // Example position and size
+        whiteDashPanel.add(tempScroll);
+
+        JPanel dashPanel = new JPanel(); //dashler alt alta olsun diye yeni center alignment yapcam
         dashPanel.setLayout(new BoxLayout(dashPanel, BoxLayout.Y_AXIS));
         
         // Number of dashes
@@ -43,11 +50,74 @@ public class Temperature extends javax.swing.JFrame {
             dashPanel.add(dashLabel);
         }
 
+        mainPanel.add(whiteDashPanel, BorderLayout.WEST);
         mainPanel.add(dashPanel, BorderLayout.EAST);
 
         // Add main panel to frame
         getContentPane().add(mainPanel);
 
         setVisible(true);
+    }
+    class TempScroll extends JPanel{
+        private BufferedImage image;
+        private int x = 0;
+        private int y = 0;
+        private int prevY = 0;
+        private boolean dragging = false;
+
+        public TempScroll() {
+            try {
+                File file = new File("/Users/beyzaasan/Downloads/button-2.png");
+                image = ImageIO.read(file);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            addMouseListener(new MouseListener() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    // Mouse basıldığında, hareket etmeye başlayın
+                    if (e.getX() >= x && e.getX() <= x + image.getWidth() && e.getY() >= y && e.getY() <= y + image.getHeight()) {
+                        dragging = true;
+                        prevY = e.getY();
+                    }
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    // Mouse bırakıldığında, hareket etmeyi durdurun
+                    dragging = false;
+                }
+
+                @Override
+                public void mouseClicked(MouseEvent e) {}
+                @Override
+                public void mouseEntered(MouseEvent e) {}
+                @Override
+                public void mouseExited(MouseEvent e) {}
+            });
+
+            addMouseMotionListener(new MouseMotionListener() {
+                @Override
+                public void mouseDragged(MouseEvent e) {
+                    if (dragging) {
+                        // Mouse hareket ederken, y koordinatını güncelle
+                        int deltaY = e.getY() - prevY;
+                        y += deltaY;
+                        prevY = e.getY();
+                        repaint();
+                    }
+                }
+
+                @Override
+                public void mouseMoved(MouseEvent e) {}
+            });
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(image, x, y, this);
+        }
     }
 }
